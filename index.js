1db592b883e5d780afe18e7fc87174e8caa71b08
mudit <mudit19.072@gmail.com>
2023-05-26 11:25:11 +0530

    first commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
 });
 
 
-
+var roompasswords=[];
 var roomtousersmapping = {};
 io.on('connection', (socket) => {
     console.log("user conncted", socket.id);
@@ -45,18 +45,34 @@ io.on('connection', (socket) => {
 
     socket.on("create_room", (data) => {
         const { username, password } = data;
+        console.log(password);
+        if(roompasswords.includes(password))
+        {
+            socket.emit("room_already_exists", { "message": "Room already exists" });
+            return;
+        }
         let url = cryptojs.AES.encrypt(password, process.env.PASS_SEC).toString();
+        roompasswords.push(password);
         socket.join(url);
         roomtousersmapping[url] = [[username,socket.id]];
+        console.log(roomtousersmapping);
         socket.emit("room_created", { url: url, users: [[username,socket.id]] });
     });
     socket.on("join_room", (data) => {
         console.log(data);
         const { username, password } = data;
+        let bytes  = cryptojs.AES.decrypt(password, process.env.PASS_SEC);
+        let originalText = bytes.toString(cryptojs.enc.Utf8);
+        if(roompasswords.indexOf(originalText)===-1)
+        {
+            socket.emit("room_not_found", { "message": "Room Not Found" });
+            return;
+        }
         socket.join(password);
         roomtousersmapping[password].push([username,socket.id]);
         socket.emit("room_joined", { url: password, users: roomtousersmapping[password] });
-        io.in(password).emit("room_data", { users: roomtousersmapping[password] });
+        socket.to(password).emit("room_data", { users: roomtousersmapping[password] });
+        socket.to(password).emit("user_joined", { user: username });
     });
 
     socket.on('disconnect', () => {
@@ -64,14 +80,18 @@ io.on('connection', (socket) => {
         for(let i=0;i<Object.keys(roomtousersmapping).length;i++){
             let key = Object.keys(roomtousersmapping)[i];
             let value = roomtousersmapping[key];
-            console.log(value);
             for(let j=0;j<value.length;j++){
                 if(value[j][1]===socket.id){
+                    socket.to(key).emit("user_disconnected", { user: value[j][0] });
                     value.splice(j,1);
-                    io.in(key).emit("room_data", { users: roomtousersmapping[key] });
+                    socket.to(key).emit("room_data", { users: roomtousersmapping[key] });
+                    
                     if(value.length===0)
                     {
-                        console.log("deleting room");
+                        let bytes  = cryptojs.AES.decrypt(key, process.env.PASS_SEC);
+                        let originalText = bytes.toString(cryptojs.enc.Utf8);
+                        roompasswords.splice(roompasswords.indexOf(originalText),1);
+                        delete roomtousersmapping[key];
                         io.sockets.adapter.rooms.delete(key);
                     }
                     break;
